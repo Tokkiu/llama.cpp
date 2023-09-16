23f62f455e8ec58fb04a6b3ee4e906140863b44b
Tokkiu <mrgao.ary@gmail.com>
2023-09-16 22:14:01 +0800

    fix

--- llama_app/llama.py
+++ llama_app/llama.py
@@ -4,23 +4,32 @@ import urllib.parse
 import requests
 import time
 import json
-from server import run_server
+from server import run_server, Downloader
 from threading import Thread
 
 app = Flask(__name__)
 
-parser = argparse.ArgumentParser(description="An example of using server.cpp with a similar API to OAI. It must be used together with server.cpp.")
-parser.add_argument("--chat-prompt", type=str, help="the top prompt in chat completions(default: 'A chat between a curious user and an artificial intelligence assistant. The assistant follows the given rules no matter what.\\n')", default='A chat between a curious user and an artificial intelligence assistant. The assistant follows the given rules no matter what.\\n')
-parser.add_argument("--user-name", type=str, help="USER name in chat completions(default: '\\nUSER: ')", default="\\nUSER: ")
-parser.add_argument("--ai-name", type=str, help="ASSISTANT name in chat completions(default: '\\nASSISTANT: ')", default="\\nASSISTANT: ")
-parser.add_argument("--system-name", type=str, help="SYSTEM name in chat completions(default: '\\nASSISTANT's RULE: ')", default="\\nASSISTANT's RULE: ")
+parser = argparse.ArgumentParser(
+    description="An example of using server.cpp with a similar API to OAI. It must be used together with server.cpp.")
+parser.add_argument("--chat-prompt", type=str,
+                    help="the top prompt in chat completions(default: 'A chat between a curious user and an artificial intelligence assistant. The assistant follows the given rules no matter what.\\n')",
+                    default='A chat between a curious user and an artificial intelligence assistant. The assistant follows the given rules no matter what.\\n')
+parser.add_argument("--user-name", type=str, help="USER name in chat completions(default: '\\nUSER: ')",
+                    default="\\nUSER: ")
+parser.add_argument("--ai-name", type=str, help="ASSISTANT name in chat completions(default: '\\nASSISTANT: ')",
+                    default="\\nASSISTANT: ")
+parser.add_argument("--system-name", type=str, help="SYSTEM name in chat completions(default: '\\nASSISTANT's RULE: ')",
+                    default="\\nASSISTANT's RULE: ")
 parser.add_argument("--stop", type=str, help="the end of response in chat completions(default: '</s>')", default="</s>")
-parser.add_argument("--llama-api", type=str, help="Set the address of server.cpp in llama.cpp(default: http://127.0.0.1:31080)", default='http://127.0.0.1:31080')
+parser.add_argument("--llama-api", type=str,
+                    help="Set the address of server.cpp in llama.cpp(default: http://127.0.0.1:31080)",
+                    default='http://127.0.0.1:31080')
 parser.add_argument("--api-key", type=str, help="Set the api key to allow only few user(default: NULL)", default="")
 parser.add_argument("--host", type=str, help="Set the ip address to listen.(default: 127.0.0.1)", default='127.0.0.1')
 parser.add_argument("--port", type=int, help="Set the port to listen.(default: 31081)", default=31081)
 
 args = parser.parse_args()
+
 
 def is_present(json, key):
     try:
@@ -30,8 +39,7 @@ def is_present(json, key):
     return True
 
 
-
-#convert chat to prompt
+# convert chat to prompt
 def convert_chat(messages):
     prompt = "" + args.chat_prompt.replace("\\n", "\n")
 
@@ -39,7 +47,6 @@ def convert_chat(messages):
     user_n = args.user_name.replace("\\n", "\n")
     ai_n = args.ai_name.replace("\\n", "\n")
     stop = args.stop.replace("\\n", "\n")
-
 
     for line in messages:
         if (line["role"] == "system"):
@@ -52,33 +59,36 @@ def convert_chat(messages):
 
     return prompt
 
+
 def make_postData(body, chat=False, stream=False):
     postData = {}
     if (chat):
         postData["prompt"] = convert_chat(body["messages"])
     else:
         postData["prompt"] = body["prompt"]
-    if(is_present(body, "temperature")): postData["temperature"] = body["temperature"]
-    if(is_present(body, "top_k")): postData["top_k"] = body["top_k"]
-    if(is_present(body, "top_p")): postData["top_p"] = body["top_p"]
-    if(is_present(body, "max_tokens")): postData["n_predict"] = body["max_tokens"]
-    if(is_present(body, "presence_penalty")): postData["presence_penalty"] = body["presence_penalty"]
-    if(is_present(body, "frequency_penalty")): postData["frequency_penalty"] = body["frequency_penalty"]
-    if(is_present(body, "repeat_penalty")): postData["repeat_penalty"] = body["repeat_penalty"]
-    if(is_present(body, "mirostat")): postData["mirostat"] = body["mirostat"]
-    if(is_present(body, "mirostat_tau")): postData["mirostat_tau"] = body["mirostat_tau"]
-    if(is_present(body, "mirostat_eta")): postData["mirostat_eta"] = body["mirostat_eta"]
-    if(is_present(body, "seed")): postData["seed"] = body["seed"]
-    if(is_present(body, "logit_bias")): postData["logit_bias"] = [[int(token), body["logit_bias"][token]] for token in body["logit_bias"].keys()]
+    if (is_present(body, "temperature")): postData["temperature"] = body["temperature"]
+    if (is_present(body, "top_k")): postData["top_k"] = body["top_k"]
+    if (is_present(body, "top_p")): postData["top_p"] = body["top_p"]
+    if (is_present(body, "max_tokens")): postData["n_predict"] = body["max_tokens"]
+    if (is_present(body, "presence_penalty")): postData["presence_penalty"] = body["presence_penalty"]
+    if (is_present(body, "frequency_penalty")): postData["frequency_penalty"] = body["frequency_penalty"]
+    if (is_present(body, "repeat_penalty")): postData["repeat_penalty"] = body["repeat_penalty"]
+    if (is_present(body, "mirostat")): postData["mirostat"] = body["mirostat"]
+    if (is_present(body, "mirostat_tau")): postData["mirostat_tau"] = body["mirostat_tau"]
+    if (is_present(body, "mirostat_eta")): postData["mirostat_eta"] = body["mirostat_eta"]
+    if (is_present(body, "seed")): postData["seed"] = body["seed"]
+    if (is_present(body, "logit_bias")): postData["logit_bias"] = [[int(token), body["logit_bias"][token]] for token in
+                                                                   body["logit_bias"].keys()]
     if (args.stop != ""):
         postData["stop"] = [args.stop]
     else:
         postData["stop"] = []
-    if(is_present(body, "stop")): postData["stop"] += body["stop"]
+    if (is_present(body, "stop")): postData["stop"] += body["stop"]
     postData["n_keep"] = -1
     postData["stream"] = stream
 
     return postData
+
 
 def make_resData(data, chat=False, promptToken=[]):
     resData = {
@@ -96,7 +106,7 @@ def make_resData(data, chat=False, promptToken=[]):
     if (len(promptToken) != 0):
         resData["promptToken"] = promptToken
     if (chat):
-        #only one choice is supported
+        # only one choice is supported
         resData["choices"] = [{
             "index": 0,
             "message": {
@@ -106,7 +116,7 @@ def make_resData(data, chat=False, promptToken=[]):
             "finish_reason": "stop" if (data["stopped_eos"] or data["stopped_word"]) else "length"
         }]
     else:
-        #only one choice is supported
+        # only one choice is supported
         resData["choices"] = [{
             "text": data["content"],
             "index": 0,
@@ -115,7 +125,8 @@ def make_resData(data, chat=False, promptToken=[]):
         }]
     return resData
 
-def make_resData_stream(data, chat=False, time_now = 0, start=False):
+
+def make_resData_stream(data, chat=False, time_now=0, start=False):
     resData = {
         "id": "chatcmpl" if (chat) else "cmpl",
         "object": "chat.completion.chunk" if (chat) else "text_completion.chunk",
@@ -130,19 +141,21 @@ def make_resData_stream(data, chat=False, time_now = 0, start=False):
     }
     if (chat):
         if (start):
-            resData["choices"][0]["delta"] =  {
+            resData["choices"][0]["delta"] = {
                 "role": "assistant"
             }
         else:
-            resData["choices"][0]["delta"] =  {
+            resData["choices"][0]["delta"] = {
                 "content": data["content"]
             }
             if (data["stop"]):
-                resData["choices"][0]["finish_reason"] = "stop" if (data["stopped_eos"] or data["stopped_word"]) else "length"
+                resData["choices"][0]["finish_reason"] = "stop" if (
+                            data["stopped_eos"] or data["stopped_word"]) else "length"
     else:
         resData["choices"][0]["text"] = data["content"]
         if (data["stop"]):
-            resData["choices"][0]["finish_reason"] = "stop" if (data["stopped_eos"] or data["stopped_word"]) else "length"
+            resData["choices"][0]["finish_reason"] = "stop" if (
+                        data["stopped_eos"] or data["stopped_word"]) else "length"
 
     return resData
 
@@ -155,13 +168,14 @@ def chat_completions():
     body = request.get_json()
     stream = False
     tokenize = False
-    if(is_present(body, "stream")): stream = body["stream"]
-    if(is_present(body, "tokenize")): tokenize = body["tokenize"]
+    if (is_present(body, "stream")): stream = body["stream"]
+    if (is_present(body, "tokenize")): tokenize = body["tokenize"]
     postData = make_postData(body, chat=True, stream=stream)
 
     promptToken = []
     if (tokenize):
-        tokenData = requests.request("POST", urllib.parse.urljoin(args.llama_api, "/tokenize"), data=json.dumps({"content": postData["prompt"]})).json()
+        tokenData = requests.request("POST", urllib.parse.urljoin(args.llama_api, "/tokenize"),
+                                     data=json.dumps({"content": postData["prompt"]})).json()
         promptToken = tokenData["tokens"]
 
     if (not stream):
@@ -173,7 +187,8 @@ def chat_completions():
         return jsonify(resData)
     else:
         def generate():
-            data = requests.request("POST", urllib.parse.urljoin(args.llama_api, "/completion"), data=json.dumps(postData), stream=True)
+            data = requests.request("POST", urllib.parse.urljoin(args.llama_api, "/completion"),
+                                    data=json.dumps(postData), stream=True)
             time_now = int(time.time())
             resData = make_resData_stream({}, chat=True, time_now=time_now, start=True)
             yield 'data: {}\n'.format(json.dumps(resData))
@@ -182,6 +197,7 @@ def chat_completions():
                     decoded_line = line.decode('utf-8')
                     resData = make_resData_stream(json.loads(decoded_line[6:]), chat=True, time_now=time_now)
                     yield 'data: {}\n'.format(json.dumps(resData))
+
         return Response(generate(), mimetype='text/event-stream')
 
 
@@ -193,13 +209,14 @@ def completion():
     body = request.get_json()
     stream = False
     tokenize = False
-    if(is_present(body, "stream")): stream = body["stream"]
-    if(is_present(body, "tokenize")): tokenize = body["tokenize"]
+    if (is_present(body, "stream")): stream = body["stream"]
+    if (is_present(body, "tokenize")): tokenize = body["tokenize"]
     postData = make_postData(body, chat=False, stream=stream)
 
     promptToken = []
     if (tokenize):
-        tokenData = requests.request("POST", urllib.parse.urljoin(args.llama_api, "/tokenize"), data=json.dumps({"content": postData["prompt"]})).json()
+        tokenData = requests.request("POST", urllib.parse.urljoin(args.llama_api, "/tokenize"),
+                                     data=json.dumps({"content": postData["prompt"]})).json()
         promptToken = tokenData["tokens"]
 
     if (not stream):
@@ -211,16 +228,49 @@ def completion():
         return jsonify(resData)
     else:
         def generate():
-            data = requests.request("POST", urllib.parse.urljoin(args.llama_api, "/completion"), data=json.dumps(postData), stream=True)
+            data = requests.request("POST", urllib.parse.urljoin(args.llama_api, "/completion"),
+                                    data=json.dumps(postData), stream=True)
             time_now = int(time.time())
             for line in data.iter_lines():
                 if line:
                     decoded_line = line.decode('utf-8')
                     resData = make_resData_stream(json.loads(decoded_line[6:]), chat=False, time_now=time_now)
                     yield 'data: {}\n'.format(json.dumps(resData))
+
         return Response(generate(), mimetype='text/event-stream')
 
+
+downloader = Downloader()
+
+
+@app.route('/model', methods=['POST'])
+def model_impl():
+    if (args.api_key != "" and request.headers["Authorization"].split()[1] != args.api_key):
+        return Response(status=403)
+    body = request.get_json()
+    model_name = "llama_13B"
+    if (is_present(body, "model")): model_name = body["model"]
+    download = 100.
+    running = True
+    if not downloader.downloaded():
+        downloader.download()
+        download = downloader.progress
+        running = False
+    else:
+        model = Thread(target=run_server, args =())
+        model.start()
+
+    return jsonify({
+        "id": "model",
+        "object": model_name,
+        "created": int(time.time()),
+        "truncated": "",
+        "model": "LLaMA_CPP",
+        "download": download,
+        "running": running
+    })
+
+
 if __name__ == '__main__':
-    model = Thread(target=run_server, args =())
-    model.start()
+    downloader.download_lib()
     app.run(args.host, port=args.port)
